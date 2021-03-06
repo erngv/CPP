c567d96d524068d6bffc4178b2a162c14cf2ab8d
Ernesto Garcia Vazquez <egv@live.unc.edu>
2021-03-06 13:34:12 -0500

    testing stuff

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -51,64 +51,64 @@ bool W(vector<vector<bool>> matrix, int i, int j)
 
 vector<vector<int>> minesweeper(vector<vector<bool>> matrix)
 {
-    vector<vector<int>> ret(matrix.size(), vector<int>(matrix[0].size(), 0));
+    vector<vector<int>> answ(matrix.size(), vector<int>(matrix[0].size(), 0));
     
     for (int i = 0; i < matrix.size(); i++)
     {
         for (int j = 0; j < matrix[0].size(); j++)
         {
-            if (NW(matrix, i, j) && matrix[i - 1][j - 1]) ret[i][j]++;
-            if ( N(matrix, i, j) && matrix[i - 1][j]    ) ret[i][j]++;
-            if (NE(matrix, i, j) && matrix[i - 1][j + 1]) ret[i][j]++;
-            if ( E(matrix, i, j) && matrix[i][j + 1]    ) ret[i][j]++;
-            if (SE(matrix, i, j) && matrix[i + 1][j + 1]) ret[i][j]++;
-            if ( S(matrix, i, j) && matrix[i + 1][j]    ) ret[i][j]++;
-            if (SW(matrix, i, j) && matrix[i + 1][j - 1]) ret[i][j]++;
-            if ( W(matrix, i, j) && matrix[i][j - 1]    ) ret[i][j]++;
+            if (NW(matrix, i, j) && matrix[i - 1][j - 1]) answ[i][j]++;
+            if ( N(matrix, i, j) && matrix[i - 1][j]    ) answ[i][j]++;
+            if (NE(matrix, i, j) && matrix[i - 1][j + 1]) answ[i][j]++;
+            if ( E(matrix, i, j) && matrix[i][j + 1]    ) answ[i][j]++;
+            if (SE(matrix, i, j) && matrix[i + 1][j + 1]) answ[i][j]++;
+            if ( S(matrix, i, j) && matrix[i + 1][j]    ) answ[i][j]++;
+            if (SW(matrix, i, j) && matrix[i + 1][j - 1]) answ[i][j]++;
+            if ( W(matrix, i, j) && matrix[i][j - 1]    ) answ[i][j]++;
         }
     }
-    return ret;
+    return answ;
 }
 
 int main()
 {
-    vector<vector<int>> ret;
+    vector<vector<int>> answ;
     
-    ret = minesweeper({{true, false, false},
-                       {false, true, false},
-                       {false, false, false}});
-    for (vector<int> v : ret)
+    answ = minesweeper({{true, false, false},
+                        {false, true, false},
+                        {false, false, false}});
+    for (vector<int> v : answ)
     {
         for (int i : v) cout << i << " ";
         cout << endl;
     }
     cout << endl;
     
-    ret = minesweeper({{true, true, true},
-                       {true, true, true},
-                       {true, true, true}});
-    for (vector<int> v : ret)
+    answ = minesweeper({{true, true, true},
+                        {true, true, true},
+                        {true, true, true}});
+    for (vector<int> v : answ)
     {
         for (int i : v) cout << i << " ";
         cout << endl;
     }
     cout << endl;
 
-    ret = minesweeper({{false, false, false},
-                       {false, false, false}});
-    for (vector<int> v : ret)
+    answ = minesweeper({{false, false, false},
+                        {false, false, false}});
+    for (vector<int> v : answ)
     {
         for (int i : v) cout << i << " ";
         cout << endl;
     }
     cout << endl;
 
-    ret = minesweeper({{true, false},
-                       {true, false},
-                       {false, true},
-                       {false, false},
-                       {false, false}});
-    for (vector<int> v : ret)
+    answ = minesweeper({{true, false},
+                        {true, false},
+                        {false, true},
+                        {false, false},
+                        {false, false}});
+    for (vector<int> v : answ)
     {
         for (int i : v) cout << i << " ";
         cout << endl;
